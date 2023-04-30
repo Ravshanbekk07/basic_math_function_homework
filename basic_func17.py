aac94e21ffd1ff5412144cfb5a6beee1c2ec9414
Ravshanbekk07 <ramazonof07@gmail.com>
2023-04-30 21:27:26 +0500

    resolvedd

--- basic_func17.py
+++ basic_func17.py
@@ -1,3 +1,21 @@
+# from math import pi
+# def main(a):
+#     '''return the value of pi
+    
+#     Args:
+#         a (float): a number
+        
+#     Returns:
+#         float: the value of pi.
+#     '''
+#     return a 
+
+# b = main(pi)
+# print(b)
+
+
+
+from math import pi
 def main():
     '''return the value of pi
     
@@ -7,4 +25,8 @@ def main():
     Returns:
         float: the value of pi.
     '''
-    return
+    a = pi
+    return a 
+
+b = main()
+print(b)
